a98dfb40b9ce32a888d65a4c0fec004f7aa4583f
mahimakhanunnisa-gif <mahimakhanunnisa@gmail.com>
2026-04-02 11:07:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -455,12 +455,27 @@ if (msg.receiver_email === user.email) {
         <h2>Users</h2>
 
         {users.map((u, i) => (
-          <div
-            key={i}
-          className={`user 
-  ${chatEmail === u.email ? "activeUser" : ""} 
-  ${unreadUsers.includes(u.email) ? "unreadUser" : ""}
-`}
+  <div
+    key={i}
+    className={`userCard 
+      ${chatEmail === u.email ? "activeUser" : ""} 
+      ${unreadUsers.includes(u.email) ? "unreadUser" : ""}
+    `}
+    onClick={() => {
+      setChatEmail(u.email);
+
+      // remove unread highlight
+      setUnreadUsers((prev) =>
+        prev.filter((email) => email !== u.email)
+      );
+    }}
+  >
+    <div className="userInfo">
+      <div className="userName">{u.name || "User"}</div>
+      <div className="userEmailSmall">({u.email})</div>
+    </div>
+  </div>
+))}
   onClick={() => {
   setChatEmail(u.email);
 
